6e5b58ffc26bc3eb47084e51621f79ba339765c1
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2024-04-18 12:48:06 +0300

    Добавил выпадающее меню

--- js/main.js
+++ js/main.js
@@ -422,12 +422,12 @@ const swiperReview = new Swiper('.swiper_review', {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 10
+      spaceBetween: 10,
     },
     // when window width is >= 480px
     580: {
       slidesPerView: 2,
-      spaceBetween: 20
+      spaceBetween: 20,
     },
     // when window width is >= 640px
     770: {
